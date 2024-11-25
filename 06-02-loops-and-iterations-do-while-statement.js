e9d971f905a59cb02481b5bb4530c247576144fb
Bryan Charles <chabry01@gmail.com>
2024-11-25 12:28:38 +0100

    Exercice résolu

--- 06-02-loops-and-iterations-do-while-statement.js
+++ 06-02-loops-and-iterations-do-while-statement.js
@@ -3,22 +3,57 @@
 // Function 1
 function sumEvenNumbers(limit) {
   // Your code here
+  let sum = 0
+  let i = 0
+
+  do{
+    if(i % 2 == 0){
+      sum += i
+    }
+    i++
+  }while(i <= limit)
+  return sum
 }
 
 // Function 2
 function printNumbersBackwards(n) {
-  // Your code here
+  // Implement a function to print numbers from 'n' to 1 in reverse order.
+  do{
+    console.log(n)
+    n--
+  }while(n >= 1)
 }
 
 // Function 3
 function reverseString(str) {
-  // Your code here
+  // Implement a function to reverse the given string.  
+  let reversedString = ''
+  let i = str.length - 1
+  do{
+    reversedString += str[i]
+    i--
+  }while(i >= 0)
+
+  return reversedString
 }
+
 
 // Function 4
 function countOccurrences(arr, val) {
-  // Your code here
+  // Implement a function to count occurrences of 'val' in the given array 'arr'.
+  let occurrences = 0
+  let i = 0
+  do{
+    if(arr[i] == val){
+      occurrences += 1
+    }
+    i++
+  }while(i < arr.length)
+
+  return occurrences
 }
+
+countOccurrences([1,2,3,4,2,8,6,2], 2)
 
 module.exports = {
   sumEvenNumbers,
